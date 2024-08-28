43ac6b4375bffe6c7cc9582fe4315456425b03a9
HoneyRao-47 <honey.23cs047@sode-edu.in>
2024-08-28 22:54:33 +0530

    Add files via upload

---
+++ J1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class J1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int start= sc.nextInt();
+        int end=sc.nextInt();
+        
+        for(int i=start;i<=end;i++){
+            for(int j=1;j<=10;j++){
+
+            
+        
+         
+
+       System.out.println(i+"x"+j+"="+(i*j));
+       
+            }
+        }
+    }
+}
+
+
