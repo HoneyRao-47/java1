b1203fc34d3d8aa0ce56beb5686fb38c862220a1
HoneyRao-47 <honey.23cs047@sode-edu.in>
2024-11-09 14:53:24 +0530

    Add files via upload

---
+++ H22.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg1;
+
+
+public class H22 {
+    public static void main(String[] args){
+        for(int i=0;i<=5;i++){
+            System.out.print(i*2+" ");
+
+        }
+        System.out.println(" ");
+        for(int i=0;i<=5;i++){
+            System.out.print(i*2+1+" ");
+        }
+        System.out.println(" ");
+        for(int j=3;j<101;j++){
+            for(int i=0;i<=10;i++){
+                System.out.print(i*j+" ");
+
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
