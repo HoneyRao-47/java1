89a0475d975eb7532f8d26ed6ed0c2a26e114461
HoneyRao-47 <honey.23cs047@sode-edu.in>
2024-11-09 15:01:43 +0530

    Add files via upload

---
+++ H28.java
@@ -0,0 +1,14 @@
+
+package pkg1;
+
+import pkg2.Tools2;
+public class H28{
+
+	public static void main(String[] args) {
+		Tools2 t1=new Tools2();
+		System.out.println(t1.reverse("Honey"));
+		
+
+	}
+
+}
