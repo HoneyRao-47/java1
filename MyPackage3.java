edae92162ee95f9aab4d89e61ca9d201e41c2d8d
HoneyRao-47 <honey.23cs047@sode-edu.in>
2024-12-10 15:29:01 +0530

    Add files via upload

---
+++ MyPackage3.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2;
+
+/**
+ *
+ * @author smv
+ */
+public class MyPackage3 {
+    public void display(){
+        System.out.println("this msg is from MyPackage class within pkg3");
+    }
+    
+}
